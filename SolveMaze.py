649bfa9a3a87ac7259749e9e42f796539d1f239c
stefanpatroi <stefanpatroi@gmail.com>
2024-09-24 11:37:22 -0400

    working product

--- SolveMaze.py
+++ SolveMaze.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from enum import Enum
+from collections import defaultdict
+from queue import Queue
 
 class MazeVisitor(ABC):
     @abstractmethod
@@ -216,6 +218,19 @@ class Path:
 
     def get_path_steps(self):
         return list(self.path)
+    
+    def __str__(self):
+        return ''.join(self.path)
+    def reverse_order(self):
+        reversed_path = []
+        for current in reversed(self.path):
+            if current == 'R':
+                reversed_path.append('L')
+            elif current == 'L':
+                reversed_path.append('R')
+            else:
+                reversed_path.append(current)
+        return Path(''.join(reversed_path))
 
 class MazeSolver(ABC):
     @abstractmethod
@@ -253,10 +268,189 @@ class RightHandSolver(MazeSolver):
 
         print(f"Path: {path.get_factorized_form()}")
         return path
+    
+class AdjacencyListBuilder(MazeVisitor):
+    def __init__(self):
+        self.adjacency_list = defaultdict(list)
+
+    def visit(self, maze: Maze):
+        size_x = maze.get_size_x()
+        size_y = maze.get_size_y()
+
+        for y in range(size_y):
+            for x in range(size_x):
+                curr_pos = Position(x, y)
+
+                if not maze.is_wall(curr_pos):
+                    adjacent_walkable_positions = []
+                    
+                    if x > 0 and not maze.is_wall(Position(x - 1, y)):
+                        adjacent_walkable_positions.append(Position(x - 1, y))
+                    if x < size_x - 1 and not maze.is_wall(Position(x + 1, y)):
+                        adjacent_walkable_positions.append(Position(x + 1, y))
+                    if y > 0 and not maze.is_wall(Position(x, y - 1)):
+                        adjacent_walkable_positions.append(Position(x, y - 1))
+                    if y < size_y - 1 and not maze.is_wall(Position(x, y + 1)):
+                        adjacent_walkable_positions.append(Position(x, y + 1))
+
+                    self.adjacency_list[curr_pos] = adjacent_walkable_positions
+
+    def get_adjacency_list(self):
+        return self.adjacency_list
+
+    def print_adjacency_list(self):
+        for current_position, adjacent_positions in self.adjacency_list.items():
+            print(f"Adjacent positions for position {current_position}: ", end="")
+            if not adjacent_positions:
+                print("None")
+            else:
+                print(" ".join(str(adjacent_position) for adjacent_position in adjacent_positions))
 
 class GraphSolver(MazeSolver):
 
+    def solve(self, maze):
+        path = Path()
+
+        adjacency_list_builder = AdjacencyListBuilder()
+        maze.accept(adjacency_list_builder)
+        adj_list = adjacency_list_builder.get_adjacency_list()
+
+        start = maze.get_start()
+        end = maze.get_end()
+
+        node_path = {}
+        to_visit = Queue()
+        visited = set()
+
+        to_visit.put(start)
+        visited.add(start)
+
+        while not to_visit.empty():
+            curr = to_visit.get()
+
+            if curr == end:
+                path = self.generate_path(node_path, start, end)
+                break
+
+            for touching in adj_list.get(curr, []):
+                if touching not in visited:
+                    visited.add(touching)
+                    to_visit.put(touching)
+                    node_path[touching] = curr
+
+        return path
+
+    def generate_path(self, node_path, start_node, end_node):
+        path = Path()
+        curr_node = end_node
+        dir = Direction.LEFT
+
+        while curr_node != start_node:
+            prev_node = node_path[curr_node]
+            want_dir = self.position_orientation(curr_node, prev_node)
+            want_turn = self.turn_choice(dir, want_dir)
+
+            if want_turn == Turn.LEFT:
+                path.add_step('L')
+            elif want_turn == Turn.RIGHT:
+                path.add_step('R')
+
+            dir = want_dir
+            curr_node = prev_node
+            path.add_step('F')
+
+        return path
+
+    def position_orientation(self, first, second):
+        first_x, first_y = first.x, first.y
+        second_x, second_y = second.x, second.y
+
+        if first_x == second_x:
+            if first_y > second_y:
+                return Direction.UP
+            else:
+                return Direction.DOWN
+        else:
+            if first_x > second_x:
+                return Direction.LEFT
+            else:
+                return Direction.RIGHT
+
+    def turn_choice(self, current, wanted_dir):
+        if current == Direction.RIGHT:
+            if wanted_dir == Direction.UP:
+                return Turn.LEFT
+            elif wanted_dir == Direction.DOWN:
+                return Turn.RIGHT
+            else:
+                return Turn.NONE
+        elif current == Direction.LEFT:
+            if wanted_dir == Direction.UP:
+                return Turn.RIGHT
+            elif wanted_dir == Direction.DOWN:
+                return Turn.LEFT
+            else:
+                return Turn.NONE
+        elif current == Direction.UP:
+            if wanted_dir == Direction.LEFT:
+                return Turn.LEFT
+            elif wanted_dir == Direction.RIGHT:
+                return Turn.RIGHT
+            else:
+                return Turn.NONE
+        else:  
+            if wanted_dir == Direction.LEFT:
+                return Turn.RIGHT
+            elif wanted_dir == Direction.RIGHT:
+                return Turn.LEFT
+            else:
+                return Turn.NONE
+
 
 def main():
+    print("** Starting Maze Runner")
+
+    file_path = input("Enter the file path of the maze: ")
+
+    try:
+        maze = Maze(file_path)
+        maze.print_maze()
+
+        validate_path = input("Do you want to validate a path? (yes/no): ").strip().lower()
+        if validate_path == 'yes':
+            path_input = input("Enter the path to be validated: ")
+            path = Path(path_input)
+            if maze.validate_path(path):
+                print("Correct path")
+            else:
+                print("Incorrect path")
+        else:
+            method = input("Enter the solving method (righthand/graph): ").strip().lower()
+            solver = None
+            if method == "righthand":
+                print("RightHand algorithm chosen.")
+                solver = RightHandSolver()
+                path = solver.solve(maze) 
+                l2r_path_f = path.get_factorized_form()
+                r2l_path_f = path.reverse_order().get_factorized_form()
+                print(f"Right to Left Solution: {r2l_path_f}")
+                print(f"Left to Right Solution: {l2r_path_f}")
+            elif method == "graph":
+                print("Graph algorithm chosen.")
+                solver = GraphSolver()
+                path2 = solver.solve(maze)
+                r2l = path2.get_factorized_form()
+                l2r = path2.reverse_order().get_factorized_form()
+                print(f"Right to Left Solution: {r2l}")
+                print(f"Left to Right Solution: {l2r}")
+            else:
+                print(f"Method '{method}' not supported.")
+                return
+        
+    except Exception as e:
+        print(f"MazeSolver failed. Reason: {e}")
+
+    print("End of MazeRunner")
+
 
 main()
